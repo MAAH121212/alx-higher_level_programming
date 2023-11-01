e83f5cac232964b42862dc186027a56ff427077b
MOH <MAAH121212>
2023-11-01 10:19:29 -0700

    pop '

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -3,5 +3,5 @@ for i in range(ord('a'), ord('z') + 1):
     if i == ord('q') or i == ord('e'):
         i += 1
         continue
-    print("{:c}".format(i), end = "")
+    print("{:c}".format(i), end="")
     i += 1
