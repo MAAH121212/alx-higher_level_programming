86985df421b28a0c25f012631d3f4cb04081bfae
MOH <MAAH121212>
2023-11-04 08:36:17 -0700

    pop

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    length = len(sys.argv) 
+    if length == 1:
+        print("0 arguments.")
+    else:
+        print("{:d} argument:".format(len(sys.argv) - 1))
+        for i, arg in enumerate(sys.argv[1:], start=1):
+            print("{:d} : {}".format(i, arg)) 
