eb28e114fb6e844d6cacfa48f1c697c4569dcfd7
MOH <MAAH121212>
2023-11-04 08:45:55 -0700

    poP

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,9 @@ if __name__ == "__main__":
     length = len(sys.argv) 
     if length == 1:
         print("0 arguments.")
+    elif length == 2:
+        print("1 argument:")
     else:
-        print("{:d} argument:".format(len(sys.argv) - 1))
+        print("{:d} arguments:".format(len(sys.argv) - 1))
         for i, arg in enumerate(sys.argv[1:], start=1):
             print("{:d}: {}".format(i, arg)) 
