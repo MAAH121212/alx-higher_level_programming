081ec390ea427041fedff7a678820ea32fee9125
MOH <MAAH121212>
2023-11-06 11:28:07 -0800

    pop

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    length = len(my_list)
-    for i in range(length):
-        i += 1
-    while i > 0:
+    if not my_list:
+        return None
+    for i in reversed(my_list):
         print("{:d}".format(i))
-        i -= 1
