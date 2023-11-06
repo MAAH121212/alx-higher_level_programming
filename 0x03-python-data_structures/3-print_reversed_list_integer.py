68e80c1e59dcce76bc5a362dcbf99e826fd9f3c0
MOH <MAAH121212>
2023-11-06 11:22:12 -0800

    pop

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    length = len(my_list)
+    for i in range(length):
+        i += 1
+    while i > 0:
+        print(my_list[i - 1])
+        i -= 1
