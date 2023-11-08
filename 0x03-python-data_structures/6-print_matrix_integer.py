60799724b8af3e418d99288428e3dad42caf3a7c
MOH <MAAH121212>
2023-11-08 12:44:36 -0800

    pop

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     if not matrix:
-        return None
-    for submatrix in matrix:
-        if len(submatrix) == 0:
-            print()
-        for i in range(len(submatrix)):
-            print("{:d}".format(submatrix[i]),
-                    end="\n" if i is len(submatrix) - 1 else " ")
+        print()
+        return
+    max_width = 0
+    for row in matrix:
+        for num in row:
+            width = len(str(num))
+            if width > max_width:
+                max_width = width
+
+    for row in matrix:
+        for num in row:
+            print("{:>{}}".format(num, max_width), end=" ")
+        print()
