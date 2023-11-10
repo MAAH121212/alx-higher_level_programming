17f93f66966cd0310c393c10e0b64d637db8d3ba
MOH <MAAH121212>
2023-11-10 06:27:41 -0800

    pop

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if is not isinstance(roman_string, str):
+    if not isinstance(roman_string, str):
         return 0
     total = 0
     num = 0
