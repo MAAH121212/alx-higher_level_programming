7d50067667f3bdf43e62c2134400bacf4e27de30
MOH <MAAH121212>
2023-11-10 06:11:41 -0800

    pop

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    maxv = 0
+    maxk = None
+    if a_dictionary is None:
+        return None
+    else:
+        for k, v in a_dictionary.items():
+            if v > maxv:
+                maxv = v
+                maxk = k
+        return maxk
