cbb6ee27315adee7bcbe14a1f80a5ae758896632
MOH <MAAH121212>
2023-11-21 17:27:49 -0800

    pop

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -8,11 +8,11 @@ def list_division(my_list_1, my_list_2, list_length):
             new_list.append(0)
             print("division by 0")
             continue
-        except IndexError :
+        except IndexError:
             new_list.append(0)
             print("out of range")
             continue
-        except TypeError :
+        except TypeError:
             new_list.append(0)
             print("wrong type")
             continue
