77bac846947ff526ddce79680f7e83fffe608c63
MOH <MAAH121212>
2023-11-22 17:05:13 -0800

    pop

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,19 +5,8 @@
 class Square:
     """Defines a square."""
     def __init__(self, size=0):
-        self.__size = size
-
-    @property
-    def size(self):
-        return self.__size
-
-    @size.setter
-    def size(self, value):
-        if not isinstance(value, int):
-            raise ValueError("size must be an integer")
-        else:
-            self.__size = value
-        if value < 0:
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = value
+        self.__size = size
