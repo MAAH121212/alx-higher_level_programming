0bb4563e6faad1cf8d30913d5f7843645e21ddbf
MOH <MAAH121212>
2023-11-22 17:49:30 -0800

    pop

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -4,8 +4,9 @@
 
 class Square:
     """Defines the area"""
-    def __init__(self, size = 0):
+    def __init__(self, size=0):
         self.__size = size
+
     @property
     def size(self):
         return self.__size
@@ -16,6 +17,8 @@ class Square:
             raise ValueError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
+
         self.__size = value
+
     def area(self):
         return self.__size * self.__size
