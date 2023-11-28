68985e4d923341124a0e8c55379eab09c2057cb7
MOH <MAAH121212>
2023-11-28 15:48:16 -0800

    pop

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""rectangle module."""
+
+
+class Rectangle:
+    """Defines a rectangle."""
+    def __init__(self, width=0, height=0):
+        self.height = height
+        self.width = width
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    def area(self):
+        return self.__height * self.__width
+
+    def perimeter(self):
+        if self.__width == 0 or self__height == 0:
+            perimeter = 0
+        return (self.__width + self.__height) * 2
