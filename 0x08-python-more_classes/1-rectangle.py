2d602228b2e624e7c2ba356accb078c90e532f27
MOH <MAAH121212>
2023-11-28 15:03:53 -0800

    pop

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -5,8 +5,8 @@
 class Rectangle:
     """Defines a rectangle."""
     def __init__(self, width=0, height=0):
-        self.__width = width
         self.__height = height
+        self.__width = width
 
     @property
     def height(self):
