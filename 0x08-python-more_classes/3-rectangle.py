38247dfae4c1e743aabc9571dd4cd7ec69e433bd
MOH <MAAH121212>
2023-11-28 16:55:37 -0800

    pop

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -43,6 +43,7 @@ class Rectangle:
     def __str__(self):
         string = ""
         if self.__width != 0 and self.__height != 0:
-            string += "\n".
-            join("#" * self.__width for i in range(self.__height))
+            for i in range(self.__height):
+                string += "#" * self.__width + "\n"
+            string = string.rstrip("\n")
         return string
