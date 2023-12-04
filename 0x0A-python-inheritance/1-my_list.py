a16ca13533038773f9b0c22209028dfe71c21440
MAAH <ma2209339@gmail.com>
2023-12-04 09:08:02 -0800

    pop

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""module for lookup method."""
+
+
+class MyList(list):
+    """that prints the list, but sorted (ascending sort)"""
+    def ___init___(self):
+        print(sorted(self))
+
