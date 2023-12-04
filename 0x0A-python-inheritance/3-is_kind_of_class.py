c46409be8259209b74c7d73943daabb1c6b090ae
MAAH <ma2209339@gmail.com>
2023-12-04 11:35:47 -0800

    pop

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""subclasses module."""
+
+
+def is_kind_of_class(obj, a_class):
+    """Write a function that returns True if the object is an instance"""
+    return isinstance(obj, a_class)
+
---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""subclasses module."""
+
+
+def inherits_from(obj, a_class):
+    """Write a function that returns True if the object is an instance"""
+     return issubclass(type(obj), a_class)
