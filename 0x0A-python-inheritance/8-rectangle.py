f11cb460ab7ab5d07e81291a55de8732083d2f0f
MAAH <ma2209339@gmail.com>
2023-12-04 12:33:59 -0800

    pop

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -18,6 +18,8 @@ class BaseGeometry:
 
 class Rectangle(BaseGeometry):
     """Rectangle Class"""
-    def __init__(self, height, width):
-        self.__height = height
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
         self.__width = width
+        self.__height = height
