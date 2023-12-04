1a960f61b1ca4bf7ffdaa6cec0ecea249855e405
MAAH <ma2209339@gmail.com>
2023-12-04 06:28:53 -0800

    pop

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -14,7 +14,7 @@ class Rectangle:
 
     @classmethod
     def square(cls, size=0):
-        return cls(size, size)
+        return cls(height=size, width=size)
 
     @property
     def height(self):
