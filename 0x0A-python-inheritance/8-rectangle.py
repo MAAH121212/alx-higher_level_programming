6fd17d14739791fd971657500feadaaed5acc6e8
MAAH <ma2209339@gmail.com>
2023-12-04 12:36:41 -0800

    pop{

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,20 +1,7 @@
 #!/usr/bin/python3
 """subclasses module."""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
-
-class BaseGeometry:
-    """Base Class"""
-
-    def area(self):
-        """error handler"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """checking value"""
-        if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
 
 class Rectangle(BaseGeometry):
     """Rectangle Class"""
