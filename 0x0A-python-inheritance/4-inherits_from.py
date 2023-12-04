fc512c76c68fb6f52618a1aa5c2dce1a1f6eaf37
MAAH <ma2209339@gmail.com>
2023-12-04 11:39:52 -0800

    pop

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,4 +4,4 @@
 
 def inherits_from(obj, a_class):
     """Write a function that returns True if the object is an instance"""
-     return issubclass(type(obj), a_class)
+    return issubclass(type(obj), a_class)
