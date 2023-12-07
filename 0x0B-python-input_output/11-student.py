5768854093196b5326375a615b45c2698b070d2d
MAAH <ma2209339@gmail.com>
2023-12-06 22:36:46 -0800

    pop

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""json module."""
+
+
+class Student:
+    """student and json"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        try:
+            for i in attrs:
+                if type(i) is not str:
+                    return self.__dict__
+        except Exception:
+            return self.__dict__
+        dict_v = dict()
+        for k, v in self.__dict__.items():
+            if k in attrs:
+                dict_v[k] = v
+        return dict_v
+
+
+    def reload_from_json(self, json):
+        for key, value in json.items():
+            if key in self.__dict__:
+                return self.__dict__[key]
