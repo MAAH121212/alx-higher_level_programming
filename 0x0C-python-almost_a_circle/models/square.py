2dbc2f89a0292f97d17f5b3fb8d1ac0134aea4d0
MAAH <ma2209339@gmail.com>
2023-12-10 08:30:16 -0800

    pop

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -10,6 +10,15 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.height = value
+        self.width = value
+
     def __str__(self):
         return "[{}] ({}) {}/{} - {}".\
             format(type(self).__name__, self.id, self.x, self.y, self.width)
