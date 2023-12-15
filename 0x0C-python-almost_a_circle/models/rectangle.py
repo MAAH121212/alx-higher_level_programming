fa8a347eb838d831b8b034f5e2814c83a26f7d32
MAAH <ma2209339@gmail.com>
2023-12-15 06:43:51 -0800

    pop

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -14,3 +14,39 @@ class Rectangle(Base):
         self.height = height
         self.x = x
         self.y = y
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        self.validator("width", value, True)  # Corrected value <= 0 to True
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        self.validator("height", value)  # Removed unnecessary argument
+        self.__height = value
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        self.validator("x", value)  # Removed unnecessary argument
+        self.__x = value
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        self.validator("y", value, False)  # Corrected False to True
+        self.__y = value
