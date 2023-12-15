ce49d92b4ae20324fea40d56a22d0e1060df3773
MAAH <ma2209339@gmail.com>
2023-12-15 06:31:36 -0800

    pop

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -9,45 +9,5 @@ class Base:
         if id is not None:
             self.id = id
         else:
-            Base.__nb_objects += 1
+            Base.__nb_objects += 1 
             self.id = Base.__nb_objects
-
-    @staticmethod
-    def to_json_string(list_dictionaries):
-        if list_dictionaries is None or not list_dictionaries:
-            return "[]"
-        return json.dumps(list_dictionaries)
-
-    @classmethod
-    def save_to_file(cls, list_objs):
-        if list_objs is not None:
-            list_obj = [o.to_dictionary() for o in list_objs]
-        with open("{}.json".format(cls.__name__), "w") as f:
-            f.write(cls.to_json_string(list_obj))
-
-    @staticmethod
-    def from_json_string(json_string):
-        if json_string is None or not json_string:
-            return []
-        return json.loads(json_string)
-
-    @classmethod
-    def create(cls, **dictionary):
-        from models/rectangle import Rectangle
-        from models/square import Square
-        if cls is Rectangle:
-            new = Rectangle(1, 1)
-        elif cls is Square:
-            new = Square(1, 1)
-        else:
-            new = None
-        return new
-
-    @classmethod
-    def load_from_file(cls):
-        from os import path
-        file = "{}.json".format(cls.__name__)
-        if not path.isfile(file):
-            return []
-        with open(file, "r") as f:
-            return [cls.create(**dicto) for dicto in cls.from_json_string(f.read())]
