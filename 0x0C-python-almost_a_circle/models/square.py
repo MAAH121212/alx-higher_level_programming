df2274e83b2af5376e9f5e76b09bf8b4c509cc9a
MAAH <ma2209339@gmail.com>
2023-12-16 02:52:46 -0800

    poP

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -5,8 +5,6 @@ from models.rectangle import Rectangle
 
 class Square(Rectangle):
     """Square identifier"""
-
-
     def __init__(self, size, x=0, y=0, id=None):
         """constructor"""
         super().__init__(size, size, x, y, id)
@@ -14,4 +12,4 @@ class Square(Rectangle):
     def __str__(self):
         """Representation"""
         return "[{}] ({}) {}/{} - {}".\
-                format(type(self).__name__, self.id, self.x, self.y, self.width)
+            format(type(self).__name__, self.id, self.x, self.y, self.width)
