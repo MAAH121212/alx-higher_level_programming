bb60ace5b9de416e1ac58e7312fe36e146bc31c6
MAAH <ma2209339@gmail.com>
2023-12-16 02:14:08 -0800

    pop

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -72,3 +72,14 @@ class Rectangle(Base):
         return "[{}] ({}) {}/{} - {}/{}".\
             format(type(self).__name__, self.id, self.__x, self.__y,
                    self.__width, self.__height)
+
+    def __update(self, id=None, size=None, x=None, y=None):
+        """update"""
+        if id is not None:
+            self.id = id
+        if size is not None:
+            self.size = size
+        if x is not None:
+            self.x = x
+        if y is not None:
+            self.y = y
