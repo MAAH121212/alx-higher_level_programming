89079317081cf4587b554f2099c1d4967e35f7c8
MAAH <ma2209339@gmail.com>
2023-12-16 03:05:05 -0800

    pop

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -18,34 +18,3 @@ class Base:
         if list_dictionaries is None:
             return "[]"
         return json.dumps(list_dictionaries)
-
-    @classmethod
-    def save_to_file(cls, list_objs):
-        list_obj = [o.to_dictionary() for o in list_objs]
-        with open("{}.json".format(cls.__name__), "w") as f:
-            f.write(cls.to_json_string(list_obj))
-
-    @staticmethod
-    def from_json_string(json_string):
-        if json_string is None or not json_string:
-            return []
-        return json.loads(json_string)
-
-    @classmethod
-    def create(cls, **dictionary):
-        if cls is Rectangle:
-            new = Rectangle(1, 1)
-        elif cls is Square:
-            new = Square(1, 1)
-        else:
-            new = None
-        return new
-
-    @classmethod
-    def load_from_file(cls):
-        from os import path
-        file = "{}.json".format(cls.__name__)
-        if not path.isfile(file):
-            return []
-        with open(file, "r") as f:
-            return [cls.create(**dicto) for dicto in cls.from_json_string(f.read()) ]
