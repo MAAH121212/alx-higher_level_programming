f4df7b4536b300121f545e55cf78ab881cd7d789
MAAH <ma2209339@gmail.com>
2023-12-16 02:51:38 -0800

    pop

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """Square module."""
-from models/rectangle import Rectangle
+from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
     """Square identifier"""
+
+
     def __init__(self, size, x=0, y=0, id=None):
         """constructor"""
         super().__init__(size, size, x, y, id)
