f1b1efb8627b2af8d583e5e7924f33fb2b77f78c
MAAH <ma2209339@gmail.com>
2023-12-16 01:27:31 -0800

    poP

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -18,6 +18,7 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, value):
+        self.validator("width", value, True)
         self.__width = value
 
     @property
@@ -26,6 +27,7 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
+        self.validator("height", value)
         self.__height = value
 
     @property
@@ -34,6 +36,7 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value):
+        self.validator("x", value)
         self.__x = value
 
     @property
@@ -42,4 +45,13 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, value):
+        self.validator("y", value, False)
         self.__y = value
+
+    def validator(self, name, value, eq=False):
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if eq and value <= 0:
+            raise ValueError("{} must be > 0".format(name))
+        elif not eq and value < 0:
+            raise ValueError("{} must be >= 0".format(name))
